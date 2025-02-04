cf6965e3b9786792fb2e436b2ae7d78fa23a083e
riya-hati <riyahati01@gmail.com git config --global user.name riya-hatigit config --global user.email riyahati01@gmail.com>
2025-02-04 23:03:07 +0530

    home page ui design

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -7,43 +7,49 @@ import Netflix from "./components/netflix/Netflix";
 // https://newsapi.org/v2/everything?q=tesla&from=2024-12-28&sortBy=publishedAt&apiKey=b6eeff707a1444cbbf11b12871b528de
 
 const Home = () => {
-  const [newsData, setNewsData] = useState([]);
-  useEffect(() => {
-    getNewsData();
-  }, []);
-  const getNewsData = async () => {
-    const response = await fetch(
-      "https://newsapi.org/v2/everything?q=tesla&from=2024-12-29&sortBy=publishedAt&apiKey=b6eeff707a1444cbbf11b12871b528de"
-    );
-    const jsonResponse = await response.json();
-    setNewsData(jsonResponse?.articles);
-  };
-  console.log(newsData);
+	const [newsData, setNewsData] = useState([]);
+	useEffect(() => {
+		getNewsData();
+	}, []);
+	const getNewsData = async () => {
+		const response = await fetch(
+			"https://newsapi.org/v2/everything?q=tesla&sortBy=publishedAt&apiKey=b6eeff707a1444cbbf11b12871b528de"
+		);
+		const jsonResponse = await response.json();
+		setNewsData(jsonResponse?.articles);
+	};
+	console.log(newsData);
 
-  return (
-    <>
-      <Welcome />
-      <Netflix />
+	return (
+		<>
+			<Welcome />
+			<Netflix />
 
-      <h1>Riya</h1>
-      <div className="container">
-        {newsData?.map((item, index) => (
-          <div className="card" key={index}>
-            <img src={item?.urlToImage} alt="image" />
-            <div className="author">
-              <h1>{item?.author}</h1>
-            </div>
-            <div className="title">
-              <h3>{item?.title}</h3>
-            </div>
-            <Link to={item?.url}>
-              <button>Read Full</button>
-            </Link>
-          </div>
-        ))}
-      </div>
-    </>
-  );
+			<div className="container">
+				{newsData?.map((item, index) => (
+					<div className="card" key={index}>
+						<img src={item?.urlToImage} alt="image" />
+						<div className="author">
+							<h1>{item?.author}</h1>
+						</div>
+						<div className="title">
+							<h3>{item?.title}</h3>
+						</div>
+						<div className="description">
+							<p>
+								{(item?.description?.slice(0, 50) ||
+									"No description available") +
+									(item?.description?.Lenghth > 60 ? "..." : "")}
+							</p>
+						</div>
+						<Link to={item?.url}>
+							<button>Read Full</button>
+						</Link>
+					</div>
+				))}
+			</div>
+		</>
+	);
 };
 
 export default Home;
--- src/pages/home/components/netflix/Netflix.jsx
+++ src/pages/home/components/netflix/Netflix.jsx
@@ -1,26 +1,34 @@
+import { ShieldQuestion } from "lucide-react";
 import MainLayout from "../../../../component/nav/layout/MainLayout";
 import "./netflix.scss";
 import React from "react";
 
 const Netflix = () => {
-  return (
-    <MainLayout>
-      <div className="netflix_section">
-        <div className="left_box">
-          <img src="../assets/john-wick.jpg" alt="images" />
-        </div>
-        <div className="Right_box">
-          <p>Netflix 12 minutes ago</p>
-          <h3>Where To Watch 'John Wick: Chapter 4'</h3>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quas
-            repudiandae saepe quos sapiente aliquam repellendus suscipit esse.
-          </p>
-          <p>Movies 4 min read</p>
-        </div>
-      </div>
-    </MainLayout>
-  );
+	return (
+		<MainLayout>
+			<div className="netflix_section">
+				<div className="left_box">
+					<img src="../assets/john-wick.jpg" alt="images" />
+				</div>
+				<div className="Right_box">
+					<p>
+						<ShieldQuestion className="icon" />
+						Netflix • 12 minutes ago
+					</p>
+					<h3>Where To Watch 'John Wick: Chapter 4'</h3>
+					<p>
+						Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque quas
+						repudiandae saepe quos sapiente aliquam repellendus suscipit esse.
+						Lorem ipsum dolor.
+					</p>
+					<p>
+						{" "}
+						<span>Movies</span> • 4 min read
+					</p>
+				</div>
+			</div>
+		</MainLayout>
+	);
 };
 
 export default Netflix;
