cf6147447fa38c597d2caf8a79fc7006ee67977f
riya-hati <riyahati01@gmail.com git config --global user.name riya-hatigit config --global user.email riyahati01@gmail.com>
2025-02-09 00:04:22 +0530

    add ham barger menu and add functionality

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -1,32 +1,62 @@
 import { Link } from "react-router-dom";
 import "./nav.scss";
-import React from "react";
+import React, { useState } from "react";
 import MainLayout from "./layout/MainLayout";
+import { CircleX, Menu } from "lucide-react";
 
 const Nav = () => {
+	const [open, setOpen] = useState(false);
 	return (
-		<MainLayout>
-			<nav className="nav_container">
-				<Link to={"/"} >
-				<img src="../assets/logo.png" alt="logo" className="logo" />
-				</Link>
-				
-				<ul>
-					<li>
-						<Link to={"/category?type=business"}>Business</Link>
-					</li>
-					<li>
-						<Link to={"/category?type=technology"}>Technology</Link>
-					</li>
-					<li>
-						<Link to={"/category?type=politics"}>Politics</Link>
-					</li>
-					<li>
-						<Link to={"/category?type=entertainment"}>Entertainment</Link>
-					</li>
-				</ul>
-			</nav>
-		</MainLayout>
+		<>
+			<MainLayout>
+				<nav className="nav_container">
+					<Link to={"/"}>
+						<img src="../assets/logo.png" alt="logo" className="logo" />
+					</Link>
+					<Menu className="ham_barger" onClick={() => setOpen(true)} />
+
+					<ul>
+						<li>
+							<Link to={"/category?type=business"}>Business</Link>
+						</li>
+						<li>
+							<Link to={"/category?type=technology"}>Technology</Link>
+						</li>
+						<li>
+							<Link to={"/category?type=politics"}>Politics</Link>
+						</li>
+						<li>
+							<Link to={"/category?type=entertainment"}>Entertainment</Link>
+						</li>
+					</ul>
+				</nav>
+			</MainLayout>
+
+			{open && (
+				<div className="side_bar">
+					<div className="wrapper">
+						<Link to={"/"}>
+							<img src="../assets/logo.png" alt="logo" className="logo-sidebar" />
+						</Link>
+						<CircleX className="circle_icon" onClick={() => setOpen(false)} />
+						<ul>
+							<li>
+								<Link to={"/category?type=business"}>Business</Link>
+							</li>
+							<li>
+								<Link to={"/category?type=technology"}>Technology</Link>
+							</li>
+							<li>
+								<Link to={"/category?type=politics"}>Politics</Link>
+							</li>
+							<li>
+								<Link to={"/category?type=entertainment"}>Entertainment</Link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			)}
+		</>
 	);
 };
 
