05da89741edff804f2760a01cd947977100e5ffd
riya-hati <riyahati01@gmail.com git config --global user.name riya-hatigit config --global user.email riyahati01@gmail.com>
2025-02-09 23:42:33 +0530

    update layouting of side bar

--- src/component/nav/Nav.jsx
+++ src/component/nav/Nav.jsx
@@ -33,26 +33,32 @@ const Nav = () => {
 			</MainLayout>
 
 			{open && (
-				<div className="side_bar">
-					<div className="wrapper">
-						<Link to={"/"}>
-							<img src="../assets/logo.png" alt="logo" className="logo-sidebar" />
-						</Link>
-						<CircleX className="circle_icon" onClick={() => setOpen(false)} />
-						<ul>
-							<li>
-								<Link to={"/category?type=business"}>Business</Link>
-							</li>
-							<li>
-								<Link to={"/category?type=technology"}>Technology</Link>
-							</li>
-							<li>
-								<Link to={"/category?type=politics"}>Politics</Link>
-							</li>
-							<li>
-								<Link to={"/category?type=entertainment"}>Entertainment</Link>
-							</li>
-						</ul>
+				<div className="sidebar_inner" onClick={() => setOpen(false)}>
+					<div className="side_bar">
+						<div className="wrapper">
+							<Link to={"/"}>
+								<img
+									src="../assets/logo-white.png"
+									alt="logo"
+									className="logo-sidebar"
+								/>
+							</Link>
+							<CircleX className="circle_icon" onClick={() => setOpen(false)} />
+							<ul>
+								<li>
+									<Link to={"/category?type=business"}>Business</Link>
+								</li>
+								<li>
+									<Link to={"/category?type=technology"}>Technology</Link>
+								</li>
+								<li>
+									<Link to={"/category?type=politics"}>Politics</Link>
+								</li>
+								<li>
+									<Link to={"/category?type=entertainment"}>Entertainment</Link>
+								</li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			)}
